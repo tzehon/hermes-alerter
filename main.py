bfcfa16ac3fe8621453fe7a28eefddf15f3a23d4
Tan Tze Hon <tth@tths-air.lan>
2020-09-27 20:47:57 +0800

    Refactor to Cloud Run

---
+++ main.py
@@ -0,0 +1,40 @@
+import requests
+import pprint
+import os
+
+from flask import Flask
+from bs4 import BeautifulSoup
+
+app = Flask(__name__)
+
+@app.route('/')
+def hermes_scraper():
+    wanted_bags = [
+        'Lindy',
+        'Birkin',
+        'Kelly',
+        'Bolide 27'
+    ]
+
+    URL = 'https://www.hermes.com/sg/en/category/women/bags-and-small-leather-goods/bags-and-clutches/'
+    page = requests.get(URL)
+
+    soup = BeautifulSoup(page.content, 'html.parser')
+
+    items = soup.find_all(class_='product-item-name')
+
+    interested_bags = []
+    for item in items:
+        available_bag = item.text.strip()
+        print("Bag on site: " + available_bag)
+        for wanted_bag in wanted_bags:
+            if wanted_bag in available_bag:
+                interested_bags.append(available_bag)
+
+    print("Wanted bags: " + str(wanted_bags))
+    print("Available bags: " + str(interested_bags))
+
+    return str(interested_bags)
+
+    if __name__ == "__main__":
+        app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
